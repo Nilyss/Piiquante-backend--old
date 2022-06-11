3c7bd909bbb593142bf6d78fe3e694b155b52945
Nicolas DECRESSAC <decressac.nicolas@gmail.com>
2022-06-11 10:18:06 +0200

    MaJ indentation

--- app.js
+++ app.js
@@ -1,31 +1,40 @@
 // Importation :
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
 
-const sauceRoutes = require('./routes/sauce');
-const userRoutes = require('./routes/user');
+const sauceRoutes = require("./routes/sauce");
+const userRoutes = require("./routes/user");
 
 const app = express();
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://n-decressac:NkrOVf4cq61yKHwL@cluster0.wmnsk.mongodb.net/?retryWrites=true&w=majority',
+mongoose
+  .connect(
+    "mongodb+srv://n-decressac:NkrOVf4cq61yKHwL@cluster0.wmnsk.mongodb.net/?retryWrites=true&w=majority",
     {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(() => console.log('Connexion à MongDB réussie !'))
-    .catch(() => console.log('Connexion à MongoDB écouchée !'));
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("Connexion à MongDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB écouchée !"));
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
 });
 
-app.use('/image', express.static(path.join(__dirname, 'images')));
-app.use('/api/sauces', sauceRoutes);
-app.use('/api/auth', userRoutes);
+app.use("/image", express.static(path.join(__dirname, "images")));
+app.use("/api/sauces", sauceRoutes);
+app.use("/api/auth", userRoutes);
 
 module.exports = app;
--- routes/sauce.js
+++ routes/sauce.js
@@ -1,16 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const auth = require('../middleware/auth');
-const multer = require('../middleware/multer-config');
+const auth = require("../middleware/auth");
+const multer = require("../middleware/multer-config");
 
-const Sauce = require('../models/sauce');
-const sauceController = require('../controllers/sauce');
+const Sauce = require("../models/sauce");
+const sauceController = require("../controllers/sauce");
 
-router.post('/', auth, multer, sauceController.createSauce);
-router.get('/', auth, sauceController.getAllsauce);
-router.get('/:id', auth, sauceController.getOneSauce);
-router.put('/:id', auth, multer, sauceController.updateOneSauce);
-router.delete('/:id', auth,  sauceController.deleteOneSauce);
+router.post("/", auth, multer, sauceController.createSauce);
+router.get("/", auth, sauceController.getAllsauce);
+router.get("/:id", auth, sauceController.getOneSauce);
+router.put("/:id", auth, multer, sauceController.updateOneSauce);
+router.delete("/:id", auth, sauceController.deleteOneSauce);
 
 module.exports = router;
